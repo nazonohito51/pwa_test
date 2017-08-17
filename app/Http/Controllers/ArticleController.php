7455cd6169664d7df99faa9a8a219a8a8b0cb18f
kawashima <nazonohito51@hotmail.com>
2017-08-17 18:57:44 +0900

    add ArticleController

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DataAccess\Eloquent\Article;
+use App\DataAccess\Eloquent\User;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index(User $user)
+    {
+        
+    }
+
+    public function create(User $user)
+    {
+        
+    }
+
+    public function store(User $user)
+    {
+        
+    }
+
+    public function show(User $user, Article $article)
+    {
+        
+    }
+
+    public function edit(User $user, Article $article)
+    {
+        
+    }
+
+    public function update(User $user, Article $article)
+    {
+        
+    }
+
+    public function destroy(User $user, Article $article)
+    {
+
+    }
+}
