837fbcfce845fa78f5b05076270267adb4ef2545
kawashima <nazonohito51@gmail.com>
2017-08-17 18:37:36 +0900

    move models to DataAccess directory

--- app/DataAccess/Eloquent/Article.php
+++ app/DataAccess/Eloquent/Article.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\DataAccess\Eloquent;
 
 use Illuminate\Database\Eloquent\Model;
 
--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\User::class,
+        'model' => \App\DataAccess\Eloquent\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
