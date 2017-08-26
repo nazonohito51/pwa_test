31523b0db295ffa44d465173a2fc3114552e3d69
kawashima <nazonohito51@hotmail.com>
2017-08-26 15:50:56 +0900

    add vue test routing

---
+++ resources/views/vue/test.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">vue test</div>
+
+                    <div id="app">
+                        @{{ hoge }}
+                        <router-link to="/top">トップページ</router-link>
+                        <router-link to="/users">ユーザー一覧ページ</router-link>
+                        <router-view></router-view>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
