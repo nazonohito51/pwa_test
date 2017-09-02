5d02aca7c5fad20fc4b11a46791f9484027fd6df
kawashima <nazonohito51@hotmail.com>
2017-09-02 14:54:56 +0900

    remove onsenUI assets in blade template

--- resources/views/layouts/onsen.blade.php
+++ resources/views/layouts/onsen.blade.php
@@ -13,11 +13,6 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    <!-- OnsenUI -->
-    <link rel="stylesheet" href="https://unpkg.com/onsenui/css/onsenui.css">
-    <link rel="stylesheet" href="https://unpkg.com/onsenui/css/onsen-css-components.min.css">
-    <script src="https://unpkg.com/onsenui/js/onsenui.min.js"></script>
-
     @yield('styles')
 </head>
 <body>
