f3dc99f57094a0143214fca1325b7ba526667a89
kawashima <nazonohito51@gmail.com>
2017-09-02 15:49:54 +0900

    change vue code location to blade

--- resources/views/front/onsenVue.blade.php
+++ resources/views/front/onsenVue.blade.php
@@ -1,9 +1,39 @@
 @extends('layouts.onsen')
 
+@section('styles')
+    <link rel="stylesheet" href="https://unpkg.com/onsenui/css/onsenui.css">
+    <link rel="stylesheet" href="https://unpkg.com/onsenui/css/onsen-css-components.min.css">
+@endsection
+
 @section('scripts')
-    <script src="{{ asset('js/onsenVue.js') }}"></script>
+    {{--<script src="{{ asset('js/onsenVue.js') }}"></script>--}}
+
+    <script src="https://unpkg.com/vue@2.4.2/dist/vue.js"></script>
+    <script src="https://unpkg.com/onsenui/js/onsenui.min.js"></script>
+    <script src="https://unpkg.com/vue-onsenui@2.1.1/dist/vue-onsenui.js"></script>
+    <script>
+        var vm = new Vue({
+            el: '#app',
+            template: '#main-page'
+        });
+    </script>
+@endsection
+
+@section('vue_templates')
+    <template id="main-page">
+        <v-ons-page>
+            <v-ons-toolbar>
+                <div class="center">Title</div>
+            </v-ons-toolbar>
+
+            <p style="text-align: center">
+                <v-ons-button @click="$ons.notification.alert('Hello World!')">
+                    Click me!
+                </v-ons-button>
+            </p>
+        </v-ons-page>
+    </template>
 @endsection
 
 @section('content')
-    @{{ hoge }}
 @endsection
