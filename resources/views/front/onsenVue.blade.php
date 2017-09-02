17c921d49553705279dbed5d7512d9d367daec69
kawashima <nazonohito51@gmail.com>
2017-09-02 16:41:11 +0900

    use v-ons-page

--- resources/views/front/onsenVue.blade.php
+++ resources/views/front/onsenVue.blade.php
@@ -12,15 +12,68 @@
     <script src="https://unpkg.com/onsenui/js/onsenui.min.js"></script>
     <script src="https://unpkg.com/vue-onsenui@2.1.1/dist/vue-onsenui.js"></script>
     <script>
+        const page2 = {
+            key: 'page2',
+            template: '#page2'
+        };
+
+        const page1 = {
+            key: 'page1',
+            template: '#page1',
+            methods: {
+                push() {
+                    this.$emit('push-page', page2);
+                }
+            }
+        };
+
         var vm = new Vue({
             el: '#app',
-            template: '#main-page'
+            template: '#main',
+            data() {
+                return {
+                    pageStack: [page1]
+                };
+            }
         });
     </script>
 @endsection
 
 @section('vue_templates')
-    <template id="main-page">
+    <template id="main">
+        @verbatim
+        <v-ons-navigator swipeable
+                         :page-stack="pageStack"
+                         @push-page="pageStack.push($event)"
+        ></v-ons-navigator>
+        @endverbatim
+    </template>
+
+    <template id="page1">
+        <v-ons-page>
+            <v-ons-toolbar>
+                <div class="center">Page 1</div>
+            </v-ons-toolbar>
+            <p style="text-align: center">
+                This is the first page
+                <v-ons-button @click="push">Push Page 2</v-ons-button>
+            </p>
+        </v-ons-page>
+    </template>
+
+    <template id="page2">
+        <v-ons-page>
+            <v-ons-toolbar>
+                <div class="left">
+                    <v-ons-back-button>Page 1</v-ons-back-button>
+                </div>
+                <div class="center">Page 2</div>
+            </v-ons-toolbar>
+            <p style="text-align: center">This is the second page</p>
+        </v-ons-page>
+    </template>
+
+    <template id="hello_button">
         <v-ons-page>
             <v-ons-toolbar>
                 <div class="center">Title</div>
