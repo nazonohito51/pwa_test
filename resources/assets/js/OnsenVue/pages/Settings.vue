2424e0bb2903361d310cd7ad115aa1bf2b123a74
kawashima <nazonohito51@gmail.com>
2017-09-03 15:46:57 +0900

    add UserSettings page

--- resources/assets/js/OnsenVue/pages/Settings.vue
+++ resources/assets/js/OnsenVue/pages/Settings.vue
@@ -5,14 +5,21 @@
         </v-ons-toolbar>
 
         <v-ons-list>
-            <v-ons-list-header>アカウント設定</v-ons-list-header>
-            <v-ons-list-item modifier="chevron" tappable>ユーザ</v-ons-list-item>
-            <v-ons-list-item modifier="chevron" tappable>通知</v-ons-list-item>
+            <v-ons-list-header>アカウント</v-ons-list-header>
+            <v-ons-list-item modifier="chevron" tappable @click="pushUserPage()">ユーザ設定</v-ons-list-item>
+            <v-ons-list-item modifier="chevron" tappable>通知設定</v-ons-list-item>
         </v-ons-list>
     </v-ons-page>
 </template>
 
 <script>
+    import User from './Settings/User.vue';
+
     export default {
+        methods: {
+            pushUserPage() {
+                this.$store.commit('navigator/push', User);
+            }
+        }
     };
 </script>
