88d5dca0ff2ef0b57fb3aed2ba405eccd2de9d85
kawashima <nazonohito51@hotmail.com>
2017-09-03 15:02:33 +0900

    add Settings page

---
+++ resources/assets/js/OnsenVue/pages/Settings.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-ons-page>
+        <v-ons-toolbar>
+            <div class="center">Settings</div>
+        </v-ons-toolbar>
+
+        <v-ons-list>
+            <v-ons-list-header>アカウント設定</v-ons-list-header>
+            <v-ons-list-item modifier="chevron" tappable>ユーザ</v-ons-list-item>
+            <v-ons-list-item modifier="chevron" tappable>通知</v-ons-list-item>
+        </v-ons-list>
+    </v-ons-page>
+</template>
+
+<script>
+    export default {
+    };
+</script>
