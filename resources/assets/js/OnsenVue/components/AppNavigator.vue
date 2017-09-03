445977df4ad171497c771761cbf5ad2e4aac78bc
kawashima <nazonohito51@hotmail.com>
2017-09-03 13:13:22 +0900

    change onsenVue code location to resources

---
+++ resources/assets/js/OnsenVue/components/AppNavigator.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-ons-navigator swipeable
+                     :page-stack="pageStack"
+                     :pop-page="storePop"
+    ></v-ons-navigator>
+</template>
+
+<script>
+    import Page1 from '../pages/Page1.vue';
+    import Page2 from '../pages/Page2.vue';
+
+    export default {
+        data () {
+            return {
+                pages: [
+                    {
+                        component: Page1,
+                        label: 'page1 label',
+                        desc: 'page1 desc.'
+                    },
+                    {
+                        component: Page2,
+                        label: 'page2 label',
+                        desc: 'page2 desc.'
+                    }
+                ]
+            };
+        },
+        beforeCreate() {
+            this.$store.commit('navigator/push', Page1);
+        },
+        computed: {
+            pageStack() {
+                return this.$store.state.navigator.stack;
+            }
+        },
+        methods: {
+            storePop() {
+                this.$store.commit('navigator/pop');
+            }
+        }
+    };
+</script>
---
+++ resources/assets/js/OnsenVue/store.js
@@ -0,0 +1,21 @@
+export default {
+    modules: {
+        navigator: {
+            strict: true,
+            namespaced: true,
+            state: {
+                stack: []
+            },
+            mutations: {
+                push(state, page) {
+                    state.stack.push(page);
+                },
+                pop(state) {
+                    if (state.stack.length > 1) {
+                        state.stack.pop();
+                    }
+                }
+            }
+        },
+    }
+};
