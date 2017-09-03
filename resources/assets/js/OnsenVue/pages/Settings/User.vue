1645eec4bf7f20890fd8cb1a05a0dbe72d71524b
kawashima <nazonohito51@hotmail.com>
2017-09-03 23:24:59 +0900

    fix user page

--- resources/assets/js/OnsenVue/pages/Settings/User.vue
+++ resources/assets/js/OnsenVue/pages/Settings/User.vue
@@ -1,6 +1,9 @@
 <template>
     <v-ons-page>
         <v-ons-toolbar>
+            <div class="left">
+                <v-ons-back-button>Settings</v-ons-back-button>
+            </div>
             <div class="center">ユーザ設定</div>
         </v-ons-toolbar>
 
@@ -11,12 +14,11 @@
                     <v-ons-icon icon="md-face" class="list-item__icon"></v-ons-icon>
                 </div>
                 <label class="center">
-                    <v-ons-input float maxlength="20"
-                                 placeholder="Name"
-                                 v-model="name"
-                    >
-                    </v-ons-input>
+                    <v-ons-input float maxlength="20" placeholder="Name" v-model="nickname"></v-ons-input>
                 </label>
+            </v-ons-list-item>
+            <v-ons-list-item v-if="nicknameChanged">
+                <v-ons-button modifier="large" @click="updateUserNickName()">更新</v-ons-button>
             </v-ons-list-item>
         </v-ons-list>
     </v-ons-page>
@@ -24,10 +26,53 @@
 
 <script>
     export default {
+        created: function () {
+            this.getUser();
+        },
         data() {
             return {
-                name: 'ほげユーザ'
+                originalNickname: null,
+                nickname: null
             };
+        },
+        methods: {
+            getUser: function() {
+                axios.get("/api/user/test").then(
+                    response => {
+                        console.log(response);
+
+                        if (response.error) {
+                            this.$ons.notification.toast('ユーザ情報の取得に失敗しました。', {timeout: 2000});
+                        } else {
+                            this.nickname = response.data.user.nickname;
+                            this.originalNickname = response.data.user.nickname;
+                        }
+                    }
+                );
+            },
+            updateUserNickName: function() {
+                axios.put("/api/user/test", {
+                    nickname: this.nickname
+                }).then(
+                    response => {
+                        console.log(response);
+
+                        if (response.error) {
+                            this.$ons.notification.toast('ニックネームの更新に失敗しました。', {timeout: 2000});
+                        } else {
+                            this.nickname = response.data.user.nickname;
+                            this.originalNickname = response.data.user.nickname;
+
+                            this.$ons.notification.toast('ニックネームを更新しました。', {timeout: 2000});
+                        }
+                    }
+                );
+            }
+        },
+        computed: {
+            nicknameChanged: function() {
+                return this.originalNickname != this.nickname;
+            }
         }
     };
 </script>
