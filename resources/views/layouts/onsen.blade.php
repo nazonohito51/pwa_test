3d1f3c22db1f68c59ef2703350aea0ba10182286
kawashima <nazonohito51@gmail.com>
2017-10-15 16:37:38 +0900

    add favicon

--- resources/views/layouts/onsen.blade.php
+++ resources/views/layouts/onsen.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" type="image/png" href="/favicon.png" sizes="48x48">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
