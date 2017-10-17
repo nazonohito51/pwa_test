383d94590d654f28b6f5b7278ca4be34d92b835a
kawashima <nazonohito51@gmail.com>
2017-10-17 22:56:29 +0900

    send push notification when post article

---
+++ app/Services/SendPushNotificationsService.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Services;
+
+use App\DataAccess\Eloquent\User;
+use Minishlink\WebPush\WebPush;
+
+class SendPushNotificationsService
+{
+    private $webPush;
+
+    public function __construct(WebPush $webPush = null)
+    {
+        $this->webPush = $webPush ? $webPush : new WebPush([
+            'VAPID' => [
+                'subject' => 'https://github.com/nazonhito51/pwa_test/',
+                'publicKey' => 'BJbwhdyPzgvLnBmxYat8cGJSck_wy0Ph_vRTPHemglPtSrmiLZ1R05yFbnfQJen-MbS97RejCn3xm6Y4v1ZvZ1Q',
+                'privateKey' => 'bOHQ5fmaaNfZiwsc2xhFFH8_iJVCsYbnfScaMPTuQQw',
+            ],
+        ]);
+    }
+
+    /**
+     * @param User|User[] $users
+     * @param string $message
+     */
+    public function execute($users, $message)
+    {
+        if ($users instanceof User) {
+            $users = [$users];
+        }
+
+        foreach ($users as $user) {
+            $this->sendPushNotification($user, $message);
+        }
+
+        $this->webPush->flush();
+    }
+
+    private function sendPushNotification(User $user, $message)
+    {
+        foreach ($user->push_notifications as $notification) {
+            $this->webPush->sendNotification(
+                $notification->endpoint,
+                $message,
+                $notification->key,
+                $notification->token,
+                true
+            );
+        }
+    }
+}
