5d0fe527c3f4c2d2359241f8e6d5890f282ed5ce
kawashima <nazonohito51@gmail.com>
2017-10-30 01:12:07 +0900

    add update UserSetting routing

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::group([], function () {
     Route::put('/user/{user}', 'Api\UserController@update');
     Route::put('/user/{user}/notification', 'Api\UserController@updateNotification');
     Route::put('/user/{user}/avator', 'Api\UserController@updateAvator');
+    Route::put('/user/{user}/setting', 'Api\UserController@updateSetting');
 
     Route::get('/articles', 'Api\ArticleController@all');
     Route::get('/articles/{article}', 'Api\ArticleController@show');
