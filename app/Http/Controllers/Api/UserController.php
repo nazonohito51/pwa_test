02c68d0b746e2589116002b6f8c661917a699854
kawashima <nazonohito51@gmail.com>
2017-10-30 00:52:40 +0900

    return user_setting when get user info

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -14,6 +14,7 @@ class UserController extends Controller
 {
     public function show(Request $request, User $user)
     {
+        $user->load('user_setting');
         return new ApiResponse(new SuccessStatus(), 'getting user is succeeded', ['user' => $user]);
     }
 
