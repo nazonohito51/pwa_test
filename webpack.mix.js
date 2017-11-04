6d1d3bd22c747f71f45eebab82b90fa587d6d069
kawashima <nazonohito51@gmail.com>
2017-11-04 17:49:35 +0900

    use sw workbox

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,56 @@
 let mix = require('laravel-mix');
+const workboxPlugin = require('workbox-webpack-plugin');
+const dist = 'public';
+
+mix.webpackConfig({
+    plugins: [
+        new workboxPlugin({
+            globDirectory: dist,
+            swDest: path.join(dist, 'sw.js'),
+            cacheId: 'pwa-test',
+            globPatterns: [
+                'manifest.json',
+                'css/**.css',
+                'images/error.png',
+                'images/avators/no_image.png',
+                'js/**.js',
+                'fonts/**/**.*',
+            ],
+            templatedUrls: {
+                '/app': ['../resources/views/layouts/onsen.blade.php', '../resources/views/front/onsenVue.blade.php']
+            },
+            runtimeCaching: [
+                {
+                    urlPattern: /images\/avators\/[^\.\/]+\.png$/,
+                    handler: 'staleWhileRevalidate',
+                    options: {
+                        cacheName: 'image-avators',
+                        cacheExpiration: {
+                            maxEntries: 10,
+                            maxAgeSeconds: 24 * 60 * 60
+                        },
+                    },
+                },
+                {
+                    urlPattern: /api\/articles\/\d+$/,
+                    handler: 'cacheFirst',
+                    options: {
+                        cacheName: 'article-details',
+                        cacheExpiration: {
+                            maxEntries: 10,
+                            maxAgeSeconds: 10 * 60
+                        },
+                    },
+                }
+            ],
+            // onsenVue.js is over 2MB(default miximumFileSize).
+            maximumFileSizeToCacheInBytes: 6 * 1024 * 1024,
+            ignoreUrlParametersMatching: [/./],
+            clientsClaim: true,
+            skipWaiting: false,
+        })
+    ]
+});
 
 /*
  |--------------------------------------------------------------------------
@@ -14,6 +66,6 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .js('resources/assets/js/onsenVue.js', 'public/js')
-    .js('resources/assets/js/main.js', 'public/js')
-    .js('resources/assets/js/ServiceWorker/pushNotification.js', 'public/js');
+    .js('resources/assets/js/main.js', 'public/js');
+    // .js('resources/assets/js/ServiceWorker/pushNotification.js', 'public/js')
     // .js('resources/assets/js/sw.js', 'public');
