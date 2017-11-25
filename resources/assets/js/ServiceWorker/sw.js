d0c04216f9e23e452879faf8a28e564307b0cced
kawashima <nazonohito51@gmail.com>
2017-11-25 22:55:59 +0900

    fix network first handler

--- resources/assets/js/ServiceWorker/sw.js
+++ resources/assets/js/ServiceWorker/sw.js
@@ -47,7 +47,7 @@ workboxSW.router.registerRoute(/images\/avatars\/[^\.\/]+\.png$/, function (args
         return response;
     });
 }, 'GET');
-workboxSW.router.registerRoute(/images\/avatars\/[^\.\/]+\.png\?self$/, function (args) {
+workboxSW.router.registerRoute(/images\/avatars\/[^\.\/]+\.png\?self&rand=[a-z0-9]{16}$/, function (args) {
     return avatar_network_first_handler.handle(args).then(function (response) {
         console.log('avatar_network_first_handler response', response);
         if (!response || response.type === 'error') {
