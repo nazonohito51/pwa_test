e983e68742468d4cf1d11ad887db7490cc20ad18
kawashima <nazonohito51@gmail.com>
2017-12-02 17:39:35 +0900

    add tabbar color

--- resources/assets/js/OnsenVue/components/AppTabbar.vue
+++ resources/assets/js/OnsenVue/components/AppTabbar.vue
@@ -1,10 +1,13 @@
 <template>
     <v-ons-page>
         <v-ons-tabbar position="auto"
+                      :modifier="md ? 'autogrow white-content' : null"
                       :tabs="tabs"
                       :visible="true"
                       :index.sync="activeIndex"
                       swipeable
+                      :on-swipe="md ? onSwipe : null"
+                      :tabbar-style="swipeTheme"
         >
         </v-ons-tabbar>
     </v-ons-page>
@@ -16,6 +19,12 @@
     import Editor from '../pages/Articles/Editor.vue';
     import Settings from '../pages/Settings.vue';
 
+    const red = [244, 67, 54];
+    const blue = [30, 136, 229];
+    const green = [20, 150, 22];
+    // Just a linear interpolation formula
+    const lerp = (x0, x1, t) => parseInt((1 - t) * x0 + t * x1, 10);
+
     export default {
         data () {
             return {
@@ -23,19 +32,24 @@
                     {
                         icon: this.md ? null : 'fa-list',
                         label: 'Timeline',
-                        page: Timeline
+                        page: Timeline,
+                        theme: red
                     },
                     {
                         icon: this.md ? null : 'fa-magic',
                         label: 'Editor',
-                        page: Editor
+                        page: Editor,
+                        theme: blue
                     },
                     {
                         icon: this.md ? null : 'fa-gear',
                         label: 'Settings',
-                        page: Settings
+                        page: Settings,
+                        theme: green
                     }
-                ]
+                ],
+                colors: red,
+                animationOptions: {}
             };
         },
         computed: {
@@ -51,8 +65,36 @@
                 return this.md && {
                     backgroundColor: `rgb(${this.colors.join(',')})`,
                     transition: `all ${this.animationOptions.duration || 0}s ${this.animationOptions.timing || ''}`
-                };
+                }
+            },
+        },
+        methods: {
+            onSwipe: function (index, animationOptions) {
+                this.animationOptions = animationOptions;
+
+                const a = Math.floor(index), b = Math.ceil(index), ratio = index % 1;
+                this.colors = this.colors.map((c, i) => {
+                    return lerp(this.tabs[a].theme[i], this.tabs[b].theme[i], ratio);
+                });
             }
         }
     };
 </script>
+
+<style>
+    /* Custom 'white-content' modifier */
+
+    .page--material .toolbar--white-content__center,
+    .page--material .toolbar-button--white-content,
+    .page--material :checked + .tabbar--white-content__button {
+        color: white;
+    }
+
+    .page--material .tabbar--white-content__button {
+        color: rgba(255, 255, 255, 0.7);
+    }
+
+    .page--material .tabbar--white-content__border {
+        background-color: white;
+    }
+</style>
